2d414d25a463bf4c3ede71cb17e336c786ead683
Manu <84148859+manugGates@users.noreply.github.com>
2021-06-02 08:53:23 -0300

    full aplication

---
+++ README.md
@@ -0,0 +1,16 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+<ul>
+<h1 align="center"> fractionSimplifier </h1></li>
+<li><p align="justify"> Adicione um numerador e um denominador que o programa te retornará a forma simplificada (se possível, irredutível) da fração.</p></li>
+<li><p align="justify" style="text-align: left;">O algoritmo foi feito apenas com "for" e "if", além da propriedade jav "DecimalFormat", porém ela é opcional. </p></li>
+<li><p align="justify" style="text-align: left;">O for acha os numeros que podem ser divididos pelo numerador e pelo denominador ao mesmo tempo, logo após o programa pega o maior divisor dessa sequência (mdc) e divide pelos 2 números, mostrando sua forma simplificada. </p></li>
+</ul>
+<p style="font-size: 10px;">Meu instagram: @fontesjoaomanoel</p>
+</body>
+</html>
---
+++ fractionSimplifier.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+ public class fractionSimplifier{
+  public static void main(String[] args){ 	
+   Scanner sc = new Scanner(System.in);   
+   DecimalFormat df = new DecimalFormat ("#");
+
+      double number = 0;
+      System.out.println ("digite o numerador");
+      double num = sc.nextDouble();
+      System.out.println ("digite o denominador");
+      int den = sc.nextInt();
+      System.out.print("divisores: ");
+
+        for (int i=0; i<=num;i++){
+          if (num%i==0){
+            if(0==den%i){
+              System.out.print(i+"; ");
+                if (i>=number){
+                  number = i;
+                }   
+           } 
+         }
+       }
+
+      System.out.println ("");
+      System.out.println ("");
+      double simplifierNum = (num/number);
+      double simplifierDen = (den/number);
+      double irreducible = (num/den);
+   System.out.print("fracao irredutivel: "+df.format(simplifierNum)+"/"+df.format(simplifierDen));
+   if (num%den==0){
+    System.out.println (" Ou: "+df.format(irreducible));
+   }
+
+  if(sc!=null){
+    sc.close();
+  }
+}
+}
