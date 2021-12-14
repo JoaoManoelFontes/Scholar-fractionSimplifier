b1f5eeb2bc26e8edf686f6fcf686e59458218766
Manu <84148859+JoaoManoelFontes@users.noreply.github.com>
2021-12-14 11:14:21 -0300

    Update fractionSimplifier.java

--- fractionSimplifier.java
+++ fractionSimplifier.java
@@ -6,29 +6,34 @@ import java.text.DecimalFormat;
    DecimalFormat df = new DecimalFormat ("#");
 
       double number = 0;
+   
       System.out.println ("digite o numerador");
       double num = sc.nextDouble();
       System.out.println ("digite o denominador");
       int den = sc.nextInt();
+   
+      System.out.println ("");
       System.out.print("divisores: ");
-
-        for (int i=0; i<=num;i++){
-          if (num%i==0){
-            if(0==den%i){
+ 
+      for (int i=0; i<=num;i++){
+          if (num%i==0 && den%i==0){             
               System.out.print(i+"; ");
-                if (i>=number){
+ 
+              if (i>=number){
                   number = i;
-                }   
-           } 
+                }               
          }
        }
 
       System.out.println ("");
       System.out.println ("");
+   
       double simplifierNum = (num/number);
       double simplifierDen = (den/number);
       double irreducible = (num/den);
+   
    System.out.print("fracao irredutivel: "+df.format(simplifierNum)+"/"+df.format(simplifierDen));
+   
    if (num%den==0){
     System.out.println (" Ou: "+df.format(irreducible));
    }
